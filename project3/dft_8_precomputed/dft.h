e6bb9bffaeaf8865dbce2e36d7072dbe2c12b55a
Sam Weismuller <sampweis@gmail.com>
2022-11-13 15:07:15 -0800

    removing old directory structure

--- project3/dft_8_precomputed/dft.h
+++
@@ -1,9 +0,0 @@
-
-typedef float DTYPE;
-typedef double IN_TYPE; // Data type for the input signal
-typedef double TEMP_TYPE; // Data type for the temporary variables
-#define N 256 // DFT Size
-//#define SIZE 8 		/* SIZE OF DFT */
-//void dft(DTYPE XX_R[SIZE], DTYPE XX_I[SIZE]);
-void dft(IN_TYPE sample_real[N], IN_TYPE sample_imag[N]);
-
